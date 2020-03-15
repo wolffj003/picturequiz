dbfef0756aa0da62983ae66527971f59c55dc92e
wolffj003 <jurrewolff@hotmail.com>
2020-03-15 14:49:16 +0100

    Completed app

---
+++ app/src/main/java/com/example/picturequizexample/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.picturequizexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        confirm.setOnClickListener() {
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer() {
+        val answer = answer.text.toString()
+
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+}
